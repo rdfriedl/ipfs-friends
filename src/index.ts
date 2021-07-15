2a1f9cfd96bfe94a4788af6ef986c60455bf0125
Robert Friedl <robert.friedl@equipmentshare.com>
2021-07-15 17:21:56 -0500

    update sync method

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,14 @@
-import { CID } from "ipfs-core";
-import path from "path";
-import fs from "fs";
-import { createMessage, encrypt, sign } from "openpgp";
-import { fetchFileBackups, updateFileBackups } from "./private-api";
 import { photoFolderPath } from "./const";
-// import { checkVersion } from "./api";
-import { getIpfs, stop } from "./ipfs";
-import { getPrivateKey, getPublicKey } from "./keys";
-import { getFileHash } from "./files";
-// import { closeAll, getLog } from "./db";
-import { syncDir } from "./sync";
-
-async function backupFiles() {
-	const ipfs = await getIpfs();
-	const fileBackups = await fetchFileBackups();
-	let newFileBackups = Array.from(fileBackups);
-
-	const folderPath = path.resolve(photoFolderPath);
-	const files = await fs.promises.readdir(folderPath);
-
-	for (const filename of files) {
-		const filePath = path.resolve(folderPath, filename);
-		const stat = await fs.promises.stat(filePath);
-
-		if (!stat.isDirectory()) {
-			const fileHash = await getFileHash(filePath);
-			const backup = newFileBackups.find((b) => b.path === filename);
-
-			if (backup && backup.fileHash !== fileHash) {
-				console.log(`Removing old file ${filePath}, old: ${backup.fileHash} new: ${fileHash}`);
-				newFileBackups = newFileBackups.filter((b) => b.fileHash !== backup.fileHash);
-				await ipfs.pin.rm(new CID(backup.ipfsHash));
-			}
-
-			if (!backup || backup.fileHash !== fileHash) {
-				const message = await createMessage({
-					filename,
-					binary: fs.createReadStream(filePath),
-				});
-				const encrypted: unknown = await encrypt({
-					message,
-					encryptionKeys: await getPublicKey(),
-					signingKeys: await getPrivateKey(),
-					armor: false,
-				});
-
-				const { cid } = await ipfs.add(encrypted as ReadableStream<Uint8Array>);
-				await ipfs.pin.add(cid);
-
-				console.log(`added ${filename} to ipfs ${cid.toString()}`);
-				newFileBackups.push({
-					path: filename,
-					ipfsHash: cid.toString(),
-					fileHash: fileHash,
-				});
-			}
-		}
-	}
-
-	await updateFileBackups(newFileBackups);
-
-	setTimeout(() => backupFiles(), 10 * 1000);
-}
+import { stop } from "./ipfs";
+import { syncLocalFolder } from "./sync";
 
 async function main() {
-	// await checkVersion();
-
-	// start ipfs client up
-	const ipfs = await getIpfs();
-	// const log = await getLog();
-	// console.log(log.address.toString());
-
-	// log.events.on('replicated', (address: string) => console.log(address))
-
-	// setInterval(() => {
-	// 	log.add(`Random number: ${Math.floor(Math.random()*1000)}`)
-	// }, 1000)
-
-	// const email = await (await (await getPublicKey()).getPrimaryUser()).user.userID?.email;
-	// if(email){
-	// 	await ipfs.pubsub.subscribe(email, (msg) => {
-	// 		console.log(msg.data.toString());
-	// 	})
-	// 	console.log(`subscribed to ${email}`);
-
-	// 	setInterval(async () => {
-	// 		const message = await createMessage({text: `random number ${Math.random()}`});
-	// 		const signed = await sign({message, signingKeys: await getPrivateKey()})
-	// 		await ipfs.pubsub.publish(`${email}/out`, Buffer.from(signed, 'utf-8'));
-	// 	}, 5000);
-	// }
-
-	// start sync process
-	// setTimeout(() => backupFiles(), 10 * 1000);
-	syncDir(path.resolve(photoFolderPath), "/files");
+	await syncLocalFolder(photoFolderPath, "/files");
 }
 main();
 
 process.on("SIGINT", async () => {
 	console.info("stopping ipfs");
-	// await closeAll();
 	await stop();
 	process.exit();
 });
--- src/sync.ts
+++ src/sync.ts
@@ -4,12 +4,21 @@ import { getIpfs, readFile as readIpfsFile } from "./ipfs";
 import { getFileHash } from "./files";
 import { createMessage, encrypt } from "openpgp";
 import { getPrivateKey, getPublicKey } from "./keys";
+import { MFSEntry } from "ipfs-core-types/src/files";
 
 const pfs = fs.promises;
 
 export type FileBackup = {
 	filename: string;
 	fileHash: string;
+};
+export type FolderBackup = {
+	name: string;
+	hash: string;
+};
+export type ipfsFolderMetadata = {
+	files: FileBackup[];
+	folders: FolderBackup[];
 };
 
 async function readAsyncIterable(iterable: AsyncIterable<Uint8Array>) {
@@ -20,29 +29,41 @@ async function readAsyncIterable(iterable: AsyncIterable<Uint8Array>) {
 	}
 	return content;
 }
+async function ipfsFilesList(ipfsPath: string) {
+	const ipfs = await getIpfs();
+	const reader = ipfs.files.ls(ipfsPath);
+	const contents: MFSEntry[] = [];
+	for await (const entery of reader) {
+		contents.push(entery);
+	}
+	return contents;
+}
 
-async function getDirMetadata(ipfsPath: string) {
+async function getIpfsFolderMetadata(ipfsPath: string) {
 	const ipfs = await getIpfs();
 	const metadataPath = path.join(ipfsPath, "metadata.json");
+	const blank: ipfsFolderMetadata = {
+		files: [],
+		folders: [],
+	};
 
 	let content;
 	try {
 		content = await readAsyncIterable(ipfs.files.read(metadataPath));
 	} catch (e) {
-		return [];
+		return blank;
 	}
 
 	// try to parse
 	try {
-		return JSON.parse(content) as FileBackup[];
-	}
-	catch(e){
+		return JSON.parse(content) as ipfsFolderMetadata;
+	} catch (e) {
 		console.log(`Failed to parse metadata.json at ${metadataPath}`);
 		console.log(e);
-		return [];
+		return blank;
 	}
 }
-async function updateDirMetadata(ipfsPath: string, metadata: FileBackup[]) {
+async function updateIpfsFolderMetadata(ipfsPath: string, metadata: ipfsFolderMetadata) {
 	const ipfs = await getIpfs();
 	await ipfs.files.write(path.join(ipfsPath, "metadata.json"), JSON.stringify(metadata, null, 2), {
 		create: true,
@@ -55,52 +76,94 @@ type BufferReadableStream = NodeJS.ReadableStream & {
 	[Symbol.asyncIterator](): AsyncIterableIterator<Buffer>;
 };
 
-export async function syncDir(folderPath: string, ipfsPath: string) {
+const ignoreFiles = ["metadata.json"];
+export async function syncLocalFolder(localPath: string, ipfsPath: string) {
+	console.log(`syncing ${localPath} to ${ipfsPath}`);
+
 	const ipfs = await getIpfs();
-	const fileBackups = await getDirMetadata(ipfsPath);
+	const metadata = await getIpfsFolderMetadata(ipfsPath);
 
-	let newFileBackups = Array.from(fileBackups);
+	const localFolderContents = await pfs.readdir(localPath, { withFileTypes: true });
+	const ipfsFolderContents = await ipfsFilesList(ipfsPath);
 
-	const files = await pfs.readdir(folderPath, { withFileTypes: true });
+	const localFiles = localFolderContents.filter((e) => e.isFile()).filter((f) => !ignoreFiles.includes(f.name));
+	const ipfsFiles = ipfsFolderContents.filter((e) => e.type === "file").filter((f) => !ignoreFiles.includes(f.name));
+	const filesTouched: string[] = [];
+	const localFolders = localFolderContents.filter((e) => e.isDirectory());
+	const ipfsFolders = ipfsFolderContents.filter((e) => e.type === "directory");
+	const foldersTouched: string[] = [];
 
-	for (const dirent of files) {
-		if (dirent.isFile()) {
-			const filename = dirent.name;
-			const filePath = path.resolve(folderPath, filename);
-			const fileHash = await getFileHash(filePath);
-			const backup = newFileBackups.find((b) => b.filename === filename);
+	// sync folders
+	for (const localFolder of localFolders) {
+		const folderLocalPath = path.join(localPath, localFolder.name);
+		const folderIpfsPath = path.join(ipfsPath, localFolder.name);
+		const meta = metadata.folders.find((f) => f.name === localFolder.name);
 
-			if (!backup || backup.fileHash !== fileHash) {
-				console.log(`${filePath} changed writing to ipfs (old ${backup?.fileHash} new ${fileHash})`);
+		if (!meta) {
+			await ipfs.files.mkdir(folderIpfsPath, { parents: true });
 
-				const message = await createMessage({
-					filename,
-					binary: fs.createReadStream(filePath),
-				});
-				const encrypted = await encrypt({
-					message,
-					encryptionKeys: await getPublicKey(),
-					signingKeys: await getPrivateKey(),
-					armor: false,
-				});
-
-				await ipfs.files.write(path.resolve(ipfsPath, filename), encrypted as BufferReadableStream, {
-					create: true,
-					parents: true,
-					truncate: true,
-				});
-
-				newFileBackups.push({
-					filename,
-					fileHash: fileHash,
-				});
-
-				await updateDirMetadata(ipfsPath, newFileBackups);
-			}
-		} else if (dirent.isDirectory()) {
-			await syncDir(path.join(folderPath, dirent.name), path.join(ipfsPath, dirent.name));
+			metadata.folders.push({
+				name: localFolder.name,
+				hash: "not-implemented",
+			});
+		}
+		foldersTouched.push(localFolder.name);
+	}
+	for (const ipfsFolder of ipfsFolders) {
+		if(!foldersTouched.includes(ipfsFolder.name)){
+			await ipfs.files.rm(path.join(ipfsPath, ipfsFolder.name));
 		}
 	}
 
-	await updateDirMetadata(ipfsPath, newFileBackups);
+	// sync files
+	while (localFiles.length > 0) {
+		const localFile = localFiles.shift() as fs.Dirent;
+		const fileLocalPath = path.join(localPath, localFile.name);
+		const fileIpfsPath = path.join(ipfsPath, localFile.name);
+		const meta = metadata.files.find((f) => f.filename === localFile.name);
+
+		const hash = await getFileHash(fileLocalPath);
+		if (!meta || meta.fileHash !== hash) {
+			const message = await createMessage({
+				filename: localFile.name,
+				binary: fs.createReadStream(fileLocalPath),
+			});
+			const encrypted = await encrypt({
+				message,
+				encryptionKeys: await getPublicKey(),
+				signingKeys: await getPrivateKey(),
+				armor: false,
+			});
+
+			await ipfs.files.write(fileIpfsPath, encrypted as BufferReadableStream, {
+				create: true,
+				truncate: true,
+			});
+
+			if (meta) {
+				meta.fileHash = localFile.name;
+				meta.fileHash = hash;
+			} else {
+				metadata.files.push({
+					filename: localFile.name,
+					fileHash: hash,
+				});
+			}
+		}
+
+		filesTouched.push(localFile.name);
+	}
+	for (const ipfsFile of ipfsFiles) {
+		if(!filesTouched.includes(ipfsFile.name)){
+			await ipfs.files.rm(path.join(ipfsPath, ipfsFile.name));
+		}
+	}
+
+	await updateIpfsFolderMetadata(ipfsPath, metadata);
+
+	for(const folder of metadata.folders){
+		await syncLocalFolder(path.join(localPath, folder.name), path.join(ipfsPath, folder.name));
+	}
+
+	return metadata;
 }
